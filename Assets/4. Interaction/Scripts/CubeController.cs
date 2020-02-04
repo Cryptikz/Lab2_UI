48e1059edd84c31469c320ed5407ce37a70f17f9
Blake <telefragent@gmail.com>
2020-02-04 10:58:41 -0500

    Initial commit

---
+++ Assets/4. Interaction/Scripts/CubeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+	public Vector3 RotationSpeed;
+
+	public void SetRotationSpeedX(float x)
+	{
+		RotationSpeed.x = x;
+	}
+	public void SetRotationSpeedY(float y)
+	{
+		RotationSpeed.y = y;
+	}
+
+	public void SetRotationSpeedZ(float z)
+	{
+		RotationSpeed.z = z;
+	}
+
+	private void Update()
+	{
+		transform.Rotate(RotationSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/4. Interaction/Scripts/CylinderController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderController : MonoBehaviour
+{
+	public float TargetSize = 1f;
+
+	public void SetSize(int sizeChoice)
+	{
+		switch(sizeChoice)
+		{
+			case 0:
+				TargetSize = 1f;
+				break;
+			case 1:
+				TargetSize = 2f;
+				break;
+			case 2:
+				TargetSize = 3f;
+				break;
+			default:
+				TargetSize = 1f;
+				break;
+		}
+	}
+
+	private void Update()
+	{
+		transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * TargetSize, Time.deltaTime);
+	}
+}
